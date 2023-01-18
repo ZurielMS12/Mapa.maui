817d630f288c03ca5eb76b6b16ae25a7f4f2e147
zurielmontanosanchez@outlook.com <zurielmontanosanchez@outlook.com>
2023-01-18 10:48:31 -0600

    Muestra la ubicación con cordenadas en puebla Mostrar la ubicación del usuario

--- Mapa/MainPage.xaml.cs
+++ Mapa/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Map = Microsoft.Maui.Controls.Maps.Map;
+﻿using Microsoft.Maui.Maps;
+using Map = Microsoft.Maui.Controls.Maps.Map;
 
 namespace Mapa;
 
@@ -6,7 +7,13 @@ public partial class MainPage : ContentPage
 {
     public MainPage()
     {
-        Map map = new Map();
+        Location location = new Location(19.04371283890263, -98.19832840321891);
+        MapSpan mapSpan = new MapSpan(location, 0.01, 0.01);
+        Map map = new Map(mapSpan)
+        //Map map = new Map()
+        {
+            IsShowingUser = true
+        };
         Content = map;
     }
 }

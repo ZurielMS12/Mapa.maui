14ceff6b187bfecb106d0b65c5b4a0ef8dc7cd56
zurielmontanosanchez@outlook.com <zurielmontanosanchez@outlook.com>
2023-01-19 00:29:40 -0600

    Cambios para modificar la etiqueta

--- Mapa/MainPage.xaml.cs
+++ Mapa/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Maui.Maps;
+﻿using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
 using Map = Microsoft.Maui.Controls.Maps.Map;
 
 namespace Mapa;
@@ -7,13 +8,44 @@ public partial class MainPage : ContentPage
 {
     public MainPage()
     {
+        Pin boardwalkPin = new Pin
+        {
+            Location = new Location(19.04371283890263, -98.19832840321891),
+            Label = "Zócalo",
+            Address = "Puebla",
+            Type = PinType.Place
+        };
+        boardwalkPin.MarkerClicked += async (s, args) =>
+        {
+            args.HideInfoWindow = true;
+            string pinName = ((Pin)s).Label;
+            await DisplayAlert("Pin Clicked", $"{pinName} was clicked.", "Ok");
+        };
+
+        Pin wharfPin = new Pin
+        {
+
+        };
+        wharfPin.InfoWindowClicked += async (s, args) =>
+        {
+            string pinName = ((Pin)s).Label;
+            await DisplayAlert("Info Window Clicked", $"The info window was clicked for {pinName}.", "Ok");
+        };
         Location location = new Location(19.04371283890263, -98.19832840321891);
         MapSpan mapSpan = new MapSpan(location, 0.01, 0.01);
         Map map = new Map(mapSpan)
-        //Map map = new Map()
+        ////Map map = new Map()
         {
             IsShowingUser = true
         };
+        Pin pin = new Pin
+        {
+            Label = "Zócalo",
+            Address = "The city Puebla",
+            Type = PinType.Place,
+            Location = new Location(19.04371283890263, -98.19832840321891)
+        };
+        map.Pins.Add(pin);
         Content = map;
     }
 }

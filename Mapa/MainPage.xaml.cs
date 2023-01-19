22a9a90183183d7d99057768c4783e0717818c74
zurielmontanosanchez@outlook.com <zurielmontanosanchez@outlook.com>
2023-01-19 16:35:17 -0600

    Interacción con un pin

--- Mapa/MainPage.xaml.cs
+++ Mapa/MainPage.xaml.cs
@@ -21,8 +21,33 @@ public partial class MainPage : ContentPage
             Type = PinType.Place,
             Location = new Location(19.0437321935132, -98.19840290899549)
         };
+        Pin boardwalkPin = new Pin
+        {
+            Location = new Location(19.0437321935132, -98.19840290899549),
+            Label = "Zocalo",
+            Address = "Ciudad de Puebla",
+            Type = PinType.Place
+        };
+        boardwalkPin.MarkerClicked += async (s, args) =>
+        {
+            args.HideInfoWindow = true;
+            string pinName = ((Pin)s).Label;
+            await DisplayAlert("Pin Clicked", $"{pinName} was clicked.", "Ok");
+        };
+
+        Pin wharfPin = new Pin
+        {
+            Location = new Location(19.047302014007155, -98.2075331478327),
+            Label = "Paseo Bravo",
+            Address = "Puebla",
+            Type = PinType.Place
+        };
+        wharfPin.InfoWindowClicked += async (s, args) =>
+        {
+            string pinName = ((Pin)s).Label;
+            await DisplayAlert("Info Window Clicked", $"The info window was clicked for {pinName}.", "Ok");
+        };
         map.Pins.Add(pin);
         Content = map;
     }
 }
-

9cad1ee01223f36679d56dfa249c619cba026a6a
zurielmontanosanchez@outlook.com <zurielmontanosanchez@outlook.com>
2023-01-19 18:09:24 -0600

    Polígonos, polilíneas y círculos

--- Mapa/MainPage.xaml.cs
+++ Mapa/MainPage.xaml.cs
@@ -8,7 +8,7 @@ public partial class MainPage : ContentPage
 {
     public MainPage()
     {
-        Location location = new Location(19.0437321935132, -98.19840290899549); 
+        Location location = new Location(19.0437321935132, -98.19840290899549);
         MapSpan mapSpan = new MapSpan(location, 0.01, 0.01);
         Map map = new Map(mapSpan)
         {
@@ -47,6 +47,50 @@ public partial class MainPage : ContentPage
             string pinName = ((Pin)s).Label;
             await DisplayAlert("Info Window Clicked", $"The info window was clicked for {pinName}.", "Ok");
         };
+        // Instantiate a polygon
+        Polygon polygon = new Polygon
+        {
+            StrokeWidth = 8,
+            StrokeColor = Color.FromArgb("#1BA1E2"),
+            FillColor = Color.FromArgb("#881BA1E2"),
+            Geopath =
+            {
+                new Location(19.0437321935132, -98.19840290899549),
+                new Location(19.04644956287260, -98.20794985340506),
+                new Location(19.040957483196564, -98.2038719820292)
+            }
+        };
+
+        // Add the polygon to the map's MapElements collection
+        map.MapElements.Add(polygon);
+
+        // instantiate a polyline
+        Polyline polyline = new Polyline
+        {
+            StrokeColor = Colors.Blue,
+            StrokeWidth = 12,
+            Geopath =
+            {
+                new Location(19.0437321935132, -98.19840290899549),
+                new Location(19.04644956287250, -98.20794985340506),
+            }
+        };
+        map.MapElements.Add(polyline);
+
+        // Instantiate a Circle
+        Circle circle = new Circle
+        {
+            Center = new Location(19.0437321935132, -98.19840290899549),
+            Radius = new Distance(250),
+            StrokeColor = Color.FromArgb("#88FF0000"),
+            StrokeWidth = 8,
+            FillColor = Color.FromArgb("#88FFC0CB")
+        };
+
+        // Add the Circle to the map's MapElements collection
+        map.MapElements.Add(circle);
+
+
         map.Pins.Add(pin);
         Content = map;
     }
